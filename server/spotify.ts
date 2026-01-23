e5055966c8715eb8cd1177ed73fca92686be3fad
Prajeet <120539751+Hughclaw18@users.noreply.github.com>
2026-01-23 15:57:53 +0530

    Updated Spotify URI
    Updated and entered the render uri for spotify

--- server/spotify.ts
+++ server/spotify.ts
@@ -4,7 +4,7 @@ import { URLSearchParams } from "url";
 const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID || "69bf2fb863af44c1b890327fb1f1efec";
 const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET || "58c63fd24c884237a3d510cfc9bd6fc0";
 // const REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI || "http://127.0.0.1:3000/api/spotify/callback";
-const REDIRECT_URI = "https://smartcradlemonitor.onrender.com/callback";
+const REDIRECT_URI = "https://smartcradlemonitor.onrender.com/api/spotify/callback";
 let accessToken: string | null = null;
 let refreshToken: string | null = null;
 let expiresIn: number = 0;
